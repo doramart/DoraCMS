b364598180fc87c241a504f15c82ea3de06f6d2b
易全文 <yiquanwen@ezbuy.com>
2017-11-17 17:11:21 +0800

    密码修改成从客户端加密，添加用户会增加用户名重复判断

--- server/lib/controller/adminUser.js
+++ server/lib/controller/adminUser.js
@@ -13,6 +13,7 @@ const validator = require('validator')
 const _ = require('lodash')
 const { service, settings, validatorUtil, logUtil, siteFunc } = require('../../../utils');
 const axios = require('axios');
+
 const pkgInfo = require('../../../package.json')
 function checkFormData(req, res, fields) {
     let errMsg = '';
@@ -25,9 +26,9 @@ function checkFormData(req, res, fields) {
     if (!validatorUtil.checkName(fields.name)) {
         errMsg = '2-6个中文字符!';
     }
-    if (!validatorUtil.checkPwd(fields.password)) {
-        errMsg = '6-12位，只能包含字母、数字和下划线!';
-    }
+    // if (!validatorUtil.checkPwd(fields.password)) {
+    //     errMsg = '6-12位，只能包含字母、数字和下划线!';
+    // }
     if (fields.password !== fields.confirmPassword) {
         errMsg = '两次输入密码不一致!';
     }
@@ -162,17 +163,16 @@ class AdminUser {
     async loginAction(req, res, next) {
         const form = new formidable.IncomingForm();
         form.parse(req, async (err, fields, files) => {
+
             let {
                 userName,
                 password
             } = fields;
             try {
-                let newPsd = service.encrypt(fields.password, settings.encrypt_key);
+
                 let errMsg = '';
                 if (!validatorUtil.checkUserName(fields.userName)) {
                     errMsg = '请输入正确的用户名'
-                } else if (!validatorUtil.checkPwd(fields.password)) {
-                    errMsg = '请输入正确的密码'
                 }
 
                 if (!fields.imageCode || fields.imageCode != req.session.imageCode) {
@@ -197,7 +197,7 @@ class AdminUser {
             }
             const userObj = {
                 userName: fields.userName,
-                password: service.encrypt(fields.password, settings.encrypt_key)
+                password: fields.password
             }
             try {
                 let user = await AdminUserModel.findOne(userObj).populate([{
@@ -285,13 +285,24 @@ class AdminUser {
                 })
                 return
             }
+            const user = await AdminUserModel.findOne({userName: fields.userName}).populate([{
+                path: 'group',
+                select: 'power _id enable name'
+            }]).exec();
+            if (user) {
+                return res.send({
+                    state: 'error',
+                    type: 'ERROR_IN_SAVE_DATA',
+                    message: `${fields.userName}已经有了，不能重复` ,
+                })
+            }
 
             const userObj = {
                 userName: fields.userName,
                 name: fields.name,
                 email: fields.email,
                 phoneNum: fields.phoneNum,
-                password: service.encrypt(fields.password, settings.encrypt_key),
+                password: fields.password,
                 confirm: fields.confirm,
                 group: fields.group,
                 enable: fields.enable,
@@ -336,7 +347,7 @@ class AdminUser {
                 name: fields.name,
                 email: fields.email,
                 phoneNum: fields.phoneNum,
-                password: service.encrypt(fields.password, settings.encrypt_key),
+                password: fields.password,
                 confirm: fields.confirm,
                 group: fields.group,
                 enable: fields.enable,
