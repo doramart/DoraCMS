852c2a3fd1b2b53f25a729093576c004b8e12bed
xiaoshen <dora@xiaoshendeMac-mini.local>
2020-11-22 22:52:23 +0800

    修复回收站分页问题

--- backstage/content/src/store/modules/content.js
+++ backstage/content/src/store/modules/content.js
@@ -1,278 +1,262 @@
-import * as types from '../types.js';
-import {
-  contentList,
-  getOneContent,
-  coverList
-} from '@/api/content';
-import _ from 'lodash';
-
+import * as types from "../types.js";
+import { contentList, getOneContent, coverList } from "@/api/content";
+import _ from "lodash";
 
 const state = {
   formState: {
     edit: false,
     formData: {
-      targetUser: '',
-      title: '',
-      stitle: '',
-      type: '1',
+      targetUser: "",
+      title: "",
+      stitle: "",
+      type: "1",
       categories: [],
-      keywords: '',
-      sortPath: '',
+      keywords: "",
+      sortPath: "",
       tags: [],
-      keywords: '',
-      sImg: '/static/upload/images/defaultImg.jpg',
-      sImgType: '2',
-      cover: '',
-      discription: '',
+      keywords: "",
+      sImg: "/static/upload/images/defaultImg.jpg",
+      sImgType: "2",
+      cover: "",
+      discription: "",
       author: {},
-      uAuthor: '',
-      markDownComments: '',
-      state: '1',
+      uAuthor: "",
+      markDownComments: "",
+      state: "1",
       isTop: 0,
-      roofPlacement: '0',
+      roofPlacement: "0",
       clickNum: 0,
-      comments: '',
-      simpleComments: '',
+      comments: "",
+      simpleComments: "",
       commentNum: 0,
       likeNum: 0,
-      dismissReason: '',
-
-    }
+      dismissReason: "",
+    },
   },
   contentList: {
     pageInfo: {},
-    docs: []
+    docs: [],
   },
   directUser: {
     formState: {
       show: false,
       edit: false,
       formData: {
-        name: '',
-        alias: '',
-        targetUser: ''
-      }
-    }
+        name: "",
+        alias: "",
+        targetUser: "",
+      },
+    },
   },
   moveCate: {
     formState: {
       show: false,
       edit: false,
       formData: {
-        categories: ''
-      }
-    }
+        categories: "",
+      },
+    },
   },
   contentCoverDialog: {
     show: false,
   },
   contentCoverList: {
     pageInfo: {},
-    docs: []
+    docs: [],
   },
   draftContentDialog: {
     show: false,
   },
   draftContentList: {
     pageInfo: {},
-    docs: []
-  }
-}
+    docs: [],
+  },
+};
 
 const mutations = {
   [types.CONTENT_FORMSTATE](state, formState) {
     state.formState.edit = formState.edit;
-    state.formState.formData = Object.assign({
-      targetUser: '',
-      title: '',
-      stitle: '',
-      type: '1',
-      categories: [],
-      keywords: '',
-      sortPath: '',
-      tags: [],
-      keywords: '',
-      sImg: '',
-      sImgType: '2',
-      cover: '',
-      discription: '',
-      author: {},
-      uAuthor: '',
-      markDownComments: '',
-      state: '1',
-      isTop: 0,
-      roofPlacement: '0',
-      clickNum: 0,
-      comments: '',
-      simpleComments: '',
-      commentNum: 0,
-      likeNum: 0
-    }, formState.formData);
-
+    state.formState.formData = Object.assign(
+      {
+        targetUser: "",
+        title: "",
+        stitle: "",
+        type: "1",
+        categories: [],
+        keywords: "",
+        sortPath: "",
+        tags: [],
+        keywords: "",
+        sImg: "",
+        sImgType: "2",
+        cover: "",
+        discription: "",
+        author: {},
+        uAuthor: "",
+        markDownComments: "",
+        state: "1",
+        isTop: 0,
+        roofPlacement: "0",
+        clickNum: 0,
+        comments: "",
+        simpleComments: "",
+        commentNum: 0,
+        likeNum: 0,
+      },
+      formState.formData
+    );
   },
   [types.CONTENT_LIST](state, contentList) {
-    state.contentList = contentList
+    state.contentList = contentList;
   },
   [types.CONTENT_ONE](state, content) {
-    state.content = content
+    state.content = content;
   },
   [types.DIRECTUSERFORMSTATE](state, formState) {
     state.directUser.formState.show = formState.show;
     state.directUser.formState.edit = formState.edit;
     state.directUser.formState.type = formState.type;
-    state.directUser.formState.formData = Object.assign({
-      name: '',
-      alias: '',
-      targetUser: ''
-    }, formState.formData);
+    state.directUser.formState.formData = Object.assign(
+      {
+        name: "",
+        alias: "",
+        targetUser: "",
+      },
+      formState.formData
+    );
   },
   [types.CONTENT_MOVECATEFORMSTATE](state, formState) {
     state.moveCate.formState.show = formState.show;
     state.moveCate.formState.edit = formState.edit;
     state.moveCate.formState.type = formState.type;
-    state.moveCate.formState.formData = Object.assign({
-      categories: ''
-    }, formState.formData);
+    state.moveCate.formState.formData = Object.assign(
+      {
+        categories: "",
+      },
+      formState.formData
+    );
   },
   [types.CONTENT_COVERDIALOGSTAGE](state, formState) {
     state.contentCoverDialog.show = formState.show;
   },
   [types.CONTENT_COVERLIST](state, contentCoverList) {
-    state.contentCoverList = contentCoverList
+    state.contentCoverList = contentCoverList;
   },
   [types.CONTENT_DRAFTDIALOGSTAGE](state, formState) {
     state.draftContentDialog.show = formState.show;
   },
   [types.CONTENT_DRAFTLIST](state, draftList) {
-    state.draftContentList = draftList
+    state.draftContentList = draftList;
   },
-}
+};
 
 const actions = {
-
-  showContentForm: ({
-    commit
-  }, params = {
-    edit: false,
-    formData: {}
-  }) => {
+  showContentForm: (
+    { commit },
+    params = {
+      edit: false,
+      formData: {},
+    }
+  ) => {
     commit(types.CONTENT_FORMSTATE, {
       edit: params.edit,
-      formData: params.formData
-    })
+      formData: params.formData,
+    });
   },
-  showDirectUserForm: ({
-    commit
-  }, params = {
-    edit: false,
-    formData: {}
-  }) => {
+  showDirectUserForm: (
+    { commit },
+    params = {
+      edit: false,
+      formData: {},
+    }
+  ) => {
     commit(types.DIRECTUSERFORMSTATE, {
       show: true,
       edit: params.edit,
-      formData: params.formData
-    })
+      formData: params.formData,
+    });
   },
-  hideDirectUserForm: ({
-    commit
-  }) => {
+  hideDirectUserForm: ({ commit }) => {
     commit(types.DIRECTUSERFORMSTATE, {
-      show: false
-    })
+      show: false,
+    });
   },
-  showMoveCateForm: ({
-    commit
-  }, params = {
-    edit: false,
-    formData: {}
-  }) => {
+  showMoveCateForm: (
+    { commit },
+    params = {
+      edit: false,
+      formData: {},
+    }
+  ) => {
     commit(types.CONTENT_MOVECATEFORMSTATE, {
       show: true,
       edit: params.edit,
-      formData: params.formData
-    })
+      formData: params.formData,
+    });
   },
-  hideMoveCateForm: ({
-    commit
-  }) => {
+  hideMoveCateForm: ({ commit }) => {
     commit(types.CONTENT_MOVECATEFORMSTATE, {
-      show: false
-    })
+      show: false,
+    });
   },
-  showCoverListDialog: ({
-    commit
-  }, params = {
-    edit: false,
-
-  }) => {
+  showCoverListDialog: (
+    { commit },
+    params = {
+      edit: false,
+    }
+  ) => {
     commit(types.CONTENT_COVERDIALOGSTAGE, {
       show: true,
-    })
+    });
   },
-  hideCoverListDialog: ({
-    commit
-  }) => {
+  hideCoverListDialog: ({ commit }) => {
     commit(types.CONTENT_COVERDIALOGSTAGE, {
-      show: false
-    })
+      show: false,
+    });
   },
-  showDraftListDialog: ({
-    commit
-  }, params = {
-    edit: false,
-
-  }) => {
+  showDraftListDialog: (
+    { commit },
+    params = {
+      edit: false,
+    }
+  ) => {
     commit(types.CONTENT_DRAFTDIALOGSTAGE, {
       show: true,
-    })
+    });
   },
-  hideDraftListDialog: ({
-    commit
-  }) => {
+  hideDraftListDialog: ({ commit }) => {
     commit(types.CONTENT_DRAFTDIALOGSTAGE, {
-      show: false
-    })
+      show: false,
+    });
   },
-  getContentList({
-    commit
-  }, params = {}) {
+  getContentList({ commit }, params = {}) {
     contentList(params).then((result) => {
-      commit(types.CONTENT_LIST, result.data)
-    })
+      commit(types.CONTENT_LIST, result.data);
+    });
   },
 
-  getOneContent({
-    commit
-  }, params = {}) {
+  getOneContent({ commit }, params = {}) {
     getOneContent(params).then((result) => {
-      commit(types.CONTENT_ONE, result.data)
-    })
+      commit(types.CONTENT_ONE, result.data);
+    });
   },
 
-  getContentCoverList({
-    commit
-  }, params = {}) {
+  getContentCoverList({ commit }, params = {}) {
     coverList(params).then((result) => {
-      commit(types.CONTENT_COVERLIST, result.data)
-    })
+      commit(types.CONTENT_COVERLIST, result.data);
+    });
   },
 
-  getDraftContentList({
-    commit
-  }, params = {
-    draft: '1'
-  }) {
-    contentList(params).then((result) => {
-      commit(types.CONTENT_DRAFTLIST, result.data)
-    })
+  getDraftContentList({ commit }, params = {}) {
+    contentList(Object.assign(params, { draft: "1" })).then((result) => {
+      commit(types.CONTENT_DRAFTLIST, result.data);
+    });
   },
-
-
-}
+};
 
 export default {
   namespaced: true,
   state,
   mutations,
-  actions
-}
+  actions,
+};
