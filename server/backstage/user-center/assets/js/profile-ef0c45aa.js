5f09c1d78ebc44525efcaa83defcb6cab6e0e170
doramart <doramart@qq.com>
2026-01-24 11:53:02 +0800

    登录入口

--- server/backstage/user-center/assets/js/profile-ef0c45aa.js
+++
@@ -1,1 +0,0 @@
-import{g as r,c as a,d as s}from"./index-3c1a6a30.js";function n(){return r("/api/v1/users/me")}function o(e){return a("/api/v1/users/me",e)}function u(e){return s("/api/v1/users/me/password",e)}function i(e){return s("/api/v1/auth/send-code",e)}export{u as c,n as g,i as s,o as u};
