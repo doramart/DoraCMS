22e618b7becbfac07eed3b0726661d4bbb1a1b42
doramart <doramart@qq.com>
2026-01-24 23:37:55 +0800

    静态资源编译

--- server/backstage/admin-center/assets/_url_-CjgJ28jF.js
+++
@@ -1,1 +0,0 @@
-import{d as s,i as a,j as o,b as r,o as t,e as n}from"./index-D8QazfP7.js";const c={class:"h-full"},l=["src"],d=s({name:"iframe-page",__name:"[url]",props:{url:{}},setup(i){return a(()=>{}),o(()=>{}),(e,u)=>(t(),r("div",c,[n("iframe",{id:"iframePage",class:"size-full",src:e.url},null,8,l)]))}});export{d as default};
---
+++ server/backstage/admin-center/assets/index-B9y2vvrM.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./exception-base.vue_vue_type_script_setup_true_lang-B2CdY7BB.js";import{d as n,c as t,o as a}from"./index-BB5cwsDr.js";const m=n({name:"500",__name:"index",setup(c){return(_,s)=>{const e=o;return a(),t(e,{type:"500"})}}});export{m as default};
